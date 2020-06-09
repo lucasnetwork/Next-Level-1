3519a15c8851f54f2feaa62174baf54180de1516
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-09 09:55:47 -0300

    add itens controller

---
+++ server/src/controllers/ItensController.ts
@@ -0,0 +1,22 @@
+import {Request,Response} from 'express'
+import knex from '../database/connection'
+
+
+class ItensController{
+    async index (request:Request, response:Response){
+        const itens = await knex('itens').select('*')
+        
+        const serializedItens = itens.map(item =>{
+            return {
+                id:item.id,
+                title:item.title,
+                image_url:`http://192.168.1.112:3333/uploads/${item.image}`
+            }
+        })
+    
+        return response.json(serializedItens)
+    
+    }
+}
+
+export default ItensController
